9954110c01e082974833ec7dd6f9f24e2175e54a
Andriy Andriychuk <33257429+Buryy13@users.noreply.github.com>
2018-02-15 23:33:27 +0200

    Update reverseNumber.js

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,17 +1,17 @@
-function reverseNumber(numb){
-	var reversedNumb = 0;
-	var isNegative = false;
-	if(numb < 0) {
-		isNegative = true;
-		numb = -numb;
-	}
-	while(numb){
-		reversedNumb *= 10;
-		reversedNumb += numb % 10;
-		numb = Math.floor(numb/10);
-	}
-	if(isNegative){
-		reversedNumb = -reversedNumb;
-	}
-	return reversedNumb;
+function reverseNumber(numb) {
+    var reversedNumb = 0;
+    var isNegative = false;
+    if (numb < 0) {
+        isNegative = true;
+        numb = -numb;
+    }
+    while (numb) {
+        reversedNumb *= 10;
+        reversedNumb += numb % 10;
+        numb = Math.floor(numb / 10);
+    }
+    if (isNegative) {
+        reversedNumb = -reversedNumb;
+    }
+    return reversedNumb;
 }
