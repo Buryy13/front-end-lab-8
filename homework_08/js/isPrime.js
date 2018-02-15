c68fd7b24c5e30db56412bc09b6160ce2bb11f61
Andriy Andriychuk <33257429+Buryy13@users.noreply.github.com>
2018-02-15 23:32:47 +0200

    Update isPrime.js

--- homework_08/js/isPrime.js
+++ homework_08/js/isPrime.js
@@ -1,9 +1,9 @@
-function isPrime(numb){
-	if(numb < 2) return false;
-	for(var i = 2; i <= Math.sqrt(numb); i++){
-		if(numb % i === 0){
-			return false;
-		}
-	}
-	return true;
+function isPrime(numb) {
+    if (numb < 2) return false;
+    for (var i = 2; i <= Math.sqrt(numb); i++) {
+        if (numb % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
