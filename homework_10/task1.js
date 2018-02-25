ee40ceaca8553310dcd69720e1b18a6531c0fa44
Andriy Andriychuk <33257429+Buryy13@users.noreply.github.com>
2018-02-25 23:10:58 +0200

    Add files via upload

---
+++ homework_10/task1.js
@@ -0,0 +1,31 @@
+function debounce(callback, delay) {
+    var timerId;
+    return function() {
+        clearTimeout(timerId);
+        timerId = setTimeout(callback, delay);
+    }
+}
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+    iterator++;
+
+    printIteratorValue();
+}
+
+function printIteratorValue() {
+    console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
